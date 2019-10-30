fed4fb7fdbbf3eae96fde587745437dee203b408
Jiovanni <{ID}+{username}@users.noreply.github.com>
2019-10-30 17:04:55 -0500

    share folder for config files, updated readme doc, add simple camera script to python scripts

--- src/mob/python_scripts/camera_script.py
+++ src/mob/python_scripts/camera_script.py
@@ -1,7 +1,9 @@
+#! /usr/bin/env python3
+
 import cv2
 
 cv2.namedWindow("preview")
-vc = cv2.VideoCapture(1)
+vc = cv2.VideoCapture(1) # configure camera to usb
 
 if vc.isOpened(): # try to get the first frame
     rval, frame = vc.read()
