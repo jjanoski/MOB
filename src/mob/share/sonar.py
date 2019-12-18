9caebe04b730cb06c9f7e45cf9f6804c95a94709
Jiovanni <{ID}+{username}@users.noreply.github.com>
2019-12-18 10:40:27 -0600

    fix sonar

--- src/mob/share/sonar.py
+++ src/mob/share/sonar.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python python
+#!/usr/bin/python3
 
 import time
 import RPi.GPIO as GPIO
@@ -9,14 +9,18 @@ class sonar:
     def __init__(self):
         self.distance_cm = None
         self.distance_inch = None
+        self.driver()
 
-    def get_cm(self):
+    def driver(self):
+        while True:
             self.distance_cm = self.read_distance(11)
-            return self.distance_cm
-
-    def get_inch(self):
-        self.distance_inch = self.distance_cm / 2.54
-        return self.distance_inch
+            self.distance_inch = self.distance_cm / 2.54
+            self.distance_cm = round(self.distance_cm, 2)
+            self.distance_inch = round(self.distance_inch, 2)
+            print("Distance: "+str(self.distance_cm)+"cm")
+            print("Distance: "+str(self.distance_inch)+"in")
+            time.sleep(2)
+        # return (self.distance_cm, self.distance_inch)
 
     def read_distance(self, pin):
         # pin configuration
@@ -47,3 +51,6 @@ class sonar:
     def get_cm_distance(self, duration):
         distance_in_cm = duration * 34000/2
         return distance_in_cm
+
+if __name__=='__main__':
+    sonar()
